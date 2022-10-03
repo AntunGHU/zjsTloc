8d2fc28b5417abee23358cc6d9bba259530a78ba
Antun <antun.jerkovic@gmail.com>
2022-10-03 13:47:27 +0200

    dkz

--- 04_ControllingProgramLogic&Flow/028_MakingDecisionsInJS.js
+++ 04_ControllingProgramLogic&Flow/028_MakingDecisionsInJS.js
@@ -2,3 +2,5 @@
 
 // o nuznosti logike malo saha ili netfliksa 
 // ciljevi: razumjeti poredbene operatore, pisati kondicionalske izjave if, else if ..., raditi s boolean operatorima
+
+console.log(!null)
