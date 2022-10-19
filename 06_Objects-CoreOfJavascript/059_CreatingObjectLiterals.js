a0367eed47de594b0e16946e5555e3178b48d5e8
Antun <antun.jerkovic@gmail.com>
2022-10-19 11:43:35 +0200

    ds

--- 06_Objects-CoreOfJavascript/059_CreatingObjectLiterals.js
+++ 06_Objects-CoreOfJavascript/059_CreatingObjectLiterals.js
@@ -13,11 +13,12 @@ const fitBitData = {
     totalMiles: 211.7,
     avgCalorieBurn: 5755,
     workoutsThisWeek: '5 of 7',
-    avgGoodSleep: '2:13'
+    avgGoodSleep: '2:13',
 };
 
 // za pozivanje valuea pisemo dot.notation
 console.log(fitBitData.totalSteps)
+console.log(fitBitData)
 
 // keysi su uvijek stringovi i pokusaji koristenja brojeva zavrsavaju pretvorbom u string (datatype symbols imaju neke izuzetke ali o tom potom). No pokusaji upisa broja zavrsavaju s brojem po displayu ali da nije broj pokazuje pokusaj dobijanja value-a za takve kljuceve kad se pojavljuje greska!!! sto je dokaz da nije spremljen medju kljuceve kao broj!
 
